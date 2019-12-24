793e3158a7da1ee5eb0207109f69a2cfab996951
Kevin Gomes <kevin28bruno@gmail.com>
2019-12-24 11:16:22 -0400

    Início do controle de sessão de usuários

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const path = require('path');
 const consign = require('consign');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+// const expressSession = require('express-session');
+// const methodOverride = require('method-override');
+// const error = require('./middlewares/error');
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+// app.use(expressSession({ resave: true, saveUninitialized: true, secret: 'ntalk' }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 consign({})
--- controllers/home.js
+++ controllers/home.js
@@ -1,8 +1,24 @@
 module.exports = (app) => {
-    const HomeController = {
-      index: function(req, res) {
-        res.render('home/index');
+  const HomeController = {
+    index(req, res) {
+      res.render('home/index');
+    },
+    login (req, res){
+      const {usuario} = req.body;
+      const {email, nome} = usuario;
+
+      if (email && nome) {
+        usuario.contatos = [];
+        req.session.usuario = usuario;
+        res.redirect('/contatos');
+      } else {
+        res.redirect('/')
       }
-    };
-    return HomeController;
+    },
+    logout(req, res){
+      req.session.destroy();
+      res.redirect('/');
+    }
   };
+  return HomeController;
+};
